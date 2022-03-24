073c8a9e7e2d6f64bb6a0c962409cc1f26ef5a62
Crjanet <99898675+Crjanet@users.noreply.github.com>
2022-03-23 20:11:36 -0400

    Add files via upload

---
+++ basicjs-assessment-variables-and-functions-Crjanet/3-temperature/temperature.js
@@ -0,0 +1,34 @@
+// Exercise Three - Temperature Converter
+//
+// Build some code which can convert from Fahrenheit to Celsius.
+// Based upon the method described here: https://www.mathsisfun.com/temperature-conversion.html
+//
+// Step One:
+//    First, ask the user with prompt() what the temperature is that they are converting.
+//    Store their answer in a variable.
+//    "What is the temperature in Fahrenheit that you want to convert to Celsius?"
+//
+// Step Two:
+//    Inside the convertFtoC() function convert the temp given from the Fahrenheit to Celsius.
+//    The formula to use is: Celsius = (Fahrenheit - 32) * 5 / 9
+//    Finally, return that value as a number.
+//
+// Step Three:
+//    Now print your answer to the browser window using document.write()
+//    It should look like:
+//    "71.6°F converted to Celsius is 22°C"
+//
+// Note that some of your numbers may have many decimal places!
+// Like 76.266666666667, that's perfectly fine!  You can leave those as is.
+//
+// 👇 WRITE YOUR CODE HERE 👇
+let temp = prompt("What is the temperature in Fahrenheit that you want to convert to Celcius?")
+
+const convertFtoC = function (temp) {
+   let Celcius = (temp - 32) * 5 / 9
+   return Celcius
+}
+completeConversion = convertFtoC(temp)
+
+let calculation = `${temp}\u00B0F converted to Celcius is ${completeConversion}\u00B0C`
+document.write(calculation)
